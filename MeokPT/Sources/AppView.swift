1a00d8887f04f8259029a5a65557e743de527afe
kdy <kokbael@naver.com>
2025-05-13 19:07:04 +0900

    Feat: AppView 에 NavigationStack 삭제, onAppear 추가하여 회원프로필 체크

--- MeokPT/Sources/AppView.swift
+++ MeokPT/Sources/AppView.swift
@@ -1,76 +1,62 @@
 import ComposableArchitecture
 import SwiftUI
 
-
 struct AppView: View {
     @Bindable var store: StoreOf<AppFeature>
     
     var body: some View {
-        NavigationStack(path: $store.path) {
-            TabView {
-                Group {
-                    DietView(store: store.scope(state: \.dietState, action: \.dietAction))
-                        .tabItem {
-                            Text("식단")
-                        }
-                    AnalyzeView(store: store.scope(state: \.analyzeState, action: \.analyzeAction))
-                        .tabItem {
-                            Text("분석")
-                        }
-                    CommunityView(store: store.scope(state: \.communityState, action: \.communityAction))
-                        .tabItem {
-                            Text("커뮤니티")
-                        }
-                    MyPageView(store: store.scope(state: \.myPageState, action: \.myPageAction))
-                        .tabItem {
-                            Text("마이페이지")
-                        }
-                }
-                .toolbarBackground(.visible, for: .tabBar)
-                .toolbarBackground(Color(UIColor.systemBackground), for: .tabBar)
-            }
-            .tint(.primary)
-            // MARK: - fullScreenCover, sheet 또는 네비게이션 생성
-            .fullScreenCover(
-                item: Binding(
-                    get: {
-                        store.appRoute?.screenType == .fullScreenCover ? store.appRoute : nil
-                    },
-                    set: { newValue in
-                        store.send(.setActiveSheet(newValue))
+        TabView {
+            Group {
+                DietView(store: store.scope(state: \.dietState, action: \.dietAction))
+                    .tabItem {
+                        Text("식단")
                     }
-                )
-            ) {_ in
-                AppSheetContentView(store: store)
-            }
-            .sheet(
-                item: Binding(
-                    get: {
-                        store.appRoute?.screenType == .sheet ? store.appRoute : nil
-                    },
-                    set: { newValue in
-                        store.send(.setActiveSheet(newValue))
+                AnalyzeView(store: store.scope(state: \.analyzeState, action: \.analyzeAction))
+                    .tabItem {
+                        Text("분석")
                     }
-                )
-            ) {_ in
-                AppSheetContentView(store: store)
-                    .presentationDragIndicator(.visible)
-                    .presentationDetents([.large])  // 여기도 나중에 분기
+                CommunityView(store: store.scope(state: \.communityState, action: \.communityAction))
+                    .tabItem {
+                        Text("커뮤니티")
+                    }
+                MyPageView(store: store.scope(state: \.myPageState, action: \.myPageAction))
+                    .tabItem {
+                        Text("마이페이지")
+                    }
             }
-//            .navigationDestination(for: AppRoute.self) { route in
-//                switch route {
-//                case .dietDetailView:
-//                    DietDetailView(store: store.scope(
-//                        state: \.dietState,
-//                        action: \.dietAction,
-//                    ))
-//                default:
-//                    Text("지원하지 않는 화면입니다")
-//                }
-//            }
+            .toolbarBackground(.visible, for: .tabBar)
+            .toolbarBackground(Color(UIColor.systemBackground), for: .tabBar)
         }
+        .tint(.primary)
         .onAppear {
             store.send(.onAppear)
+        }
+        // MARK: - fullScreenCover, sheet 또는 네비게이션 생성
+        .fullScreenCover(
+            item: Binding(
+                get: {
+                    store.appRoute?.screenType == .fullScreenCover ? store.appRoute : nil
+                },
+                set: { newValue in
+                    store.send(.setActiveSheet(newValue))
+                }
+            )
+        ) {_ in
+            AppSheetContentView(store: store)
+        }
+        .sheet(
+            item: Binding(
+                get: {
+                    store.appRoute?.screenType == .sheet ? store.appRoute : nil
+                },
+                set: { newValue in
+                    store.send(.setActiveSheet(newValue))
+                }
+            )
+        ) {_ in
+            AppSheetContentView(store: store)
+                .presentationDragIndicator(.visible)
+                .presentationDetents([.large])  // 여기도 나중에 분기
         }
     }
 }
