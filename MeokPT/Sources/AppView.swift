ff845c44bdbd7437b164cbba9279903f87f8e897
kdy <kokbael@naver.com>
2025-05-14 10:08:08 +0900

    Fix: Merge 누락 부분 수정

--- MeokPT/Sources/AppView.swift
+++ MeokPT/Sources/AppView.swift
@@ -11,7 +11,7 @@ struct AppView: View {
                     .tabItem {
                         Text("식단")
                     }
-                AnalyzeView(store: store.scope(state: \.analyzeState, action: \.analyzeAction))
+                DailyNutritionDietInfoView(store: store.scope(state: \.analyzeState, action: \.analyzeAction))
                     .tabItem {
                         Text("분석")
                     }
