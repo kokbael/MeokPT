da44f12574c4120d13675882293453626ffad56b
sion0104 <sio0104@naver.com>
2025-05-28 15:11:26 +0900

    style: 기존 View 컴포넌트 UI/UX 개선

--- MeokPT/Sources/Analyze/View/Conditional/Diet/DietEmptyView.swift
+++ MeokPT/Sources/Analyze/View/Conditional/Diet/DietEmptyView.swift
@@ -1,14 +1,47 @@
 import SwiftUI
 
 struct DietEmptyView: View {
+//    var onAddDietButtonTap: () -> Void
+
     var body: some View {
-        // MARK: - 식단이 없는 경우
-        Text("추가한 식단이 없습니다")
-            .foregroundStyle(Color.secondary)
-        Spacer()
+        VStack(spacing: 25) {
+            Image(systemName: "fork.knife.circle.fill")
+                .font(.system(size: 50))
+                .foregroundColor(Color("AppTintColor"))
+
+            VStack(spacing: 10) {
+                Text("오늘의 식단을 기록해보세요")
+                    .font(.headline)
+                    .foregroundStyle(Color.secondary)
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.horizontal)
+
+//            Button {
+//                onAddDietButtonTap()
+//            } label: {
+//                Text("식단 추가하기")
+//                    .fontWeight(.semibold)
+//                    .padding()
+//                    .frame(maxWidth: .infinity)
+//                    .foregroundColor(.white)
+//                    .background(Color("AppTintColor"))
+//                    .cornerRadius(12)
+//            }
+//            .padding(.horizontal, 40)
+//            .padding(.top, 10)
+        }
+        .padding(.vertical, 30)
+        .frame(maxWidth: .infinity)
     }
 }
 
 #Preview {
-    DietEmptyView()
+    VStack { 
+        Spacer()
+        DietEmptyView()
+        Spacer()
+    }
+    .background(Color(UIColor.systemGroupedBackground)) // 배경색 추가
+    .edgesIgnoringSafeArea(.all)
 }
